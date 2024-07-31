b688cc117d2b57c786698871caacad8cd72f70a5
Batuhan Güleç <batuhan.gulec170@gmail.com>
2024-07-31 19:40:03 +0300

    config update

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "atmvjp",
+    projectId: "hg34x2",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
